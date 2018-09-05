a5f4790f58f397aada5cfeab08c83dad2eb551c9
IMRON REVIADY <imronreviady2@gmail.com>
2018-09-05 09:36:19 +0700

    add view and logic to login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import { store } from './store.js'
+import './assets/scss/app.scss'
 
 const fb = require('./firebaseConfig.js')
 
